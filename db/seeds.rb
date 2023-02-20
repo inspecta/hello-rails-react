23eddcc2dd647dd92c8de2ab1c8b5d4d2cee96a0
Derrick Mulinde <djmulinde@gmail.com>
2023-02-20 22:14:52 +0300

    Add migration and schema

---
+++ db/seeds.rb
@@ -0,0 +1,12 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+message1 = Message.create(greetings: "Holla, Amigo!")
+message2 = Message.create(greetings: "Gyebaleko")
+message3 = Message.create(greetings: "Whatsup bro?")
+message4 = Message.create(greetings: "Osibyeyo Otya?")
+message5 = Message.create(greetings: "Coma estas?")
